5b277ff8f769aace0e4926f85c229b633fe8b591
Shiv Muthukumar <shivsmk.pr@gmail.com>
2022-04-10 12:34:18 +0800

    Update sudoku_solver.py

--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -12,10 +12,10 @@ class Sudoku:
     input_file: str
 
     def __post_init__(self):
-        # Setup sudoku board with 0 being empty
+        # SETUP SUDOKU BOARD WITH 0 BEING EMPTY
         self.board: np.ndarray = np.zeros((9, 9))
 
-        # Read board from file
+        # READ BOARD FROM FILE
         __location__ = path.realpath(path.join(getcwd(), path.dirname(__file__)))
         with open(path.join(__location__, self.input_file), encoding="utf8") as file:
             row = 0
@@ -27,9 +27,11 @@ class Sudoku:
                     col += 1
                 row += 1
 
-        # A full set of 9 numbers
+        # A SET OF ALL NUMBERS 1-9 (INCLUSIVE)
         self.set = set(range(1, 10))
 
+    # FIND ALLOWED NUMBERS BASED ON THE CORRESPONDING 3X3 SUDOKU SUB-GRID
+    # THAT THE GIVEN CELL IS A PART OFF
     def square_find(self, row: int, col: int) -> set:
         x_off = 3 * floor(row / 3)
         y_off = 3 * floor(col / 3)
@@ -40,6 +42,7 @@ class Sudoku:
                     square_set.add(self.board[i + x_off][j + y_off])
         return self.set - square_set
 
+    # FIND ALLOWED NUMBERS BASED ON THE CELL'S ROW
     def row_find(self, row: int) -> set:
         row_set = set()
         for i in range(9):
@@ -47,6 +50,7 @@ class Sudoku:
                 row_set.add(self.board[row][i])
         return self.set - row_set
 
+    # FIND ALLOWED NUMBERS BASED ON THE CELL'S COLUMN
     def col_find(self, col: int) -> set:
         col_set = set()
         for i in range(9):
@@ -54,6 +58,7 @@ class Sudoku:
                 col_set.add(self.board[i][col])
         return self.set - col_set
 
+    # FIND ALL THE ALLOWED NUMBERS GIVEN A CELL
     def get_allowed_numbers(self, row: int, col: int) -> list:
         return list(
             set.intersection(
@@ -63,7 +68,10 @@ class Sudoku:
             )
         )
 
-    def solver_distinct(self):
+    # SOLVE BY FINDING AND FILLLING ALL THE CELLS
+    # THAT CAN ONLY TAKE 1 POSSIBLE NUMBER
+    # DUE TO THE CONSTRAINTS PROVIDED
+    def solver_distinct(self) -> None:
         solve: bool = True
         while solve:
             has_change: bool = False
@@ -77,7 +85,10 @@ class Sudoku:
             if not has_change:
                 solve = False
 
-    def solver_eliminate(self):
+    # SOLVE BY FINDING AND FILLING CELLS
+    # IN A 3X3 GRID BY ELIMINATING THE ALLOWED NUMBERS
+    # TILL ONLY 1 NUMBER IS POSSIBLE FOR THE CELL
+    def solver_eliminate(self) -> None:
         solve: bool = True
         while solve:
             has_change: bool = False
@@ -105,25 +116,30 @@ class Sudoku:
             if not has_change:
                 solve = False
 
-    def solve(self, brute: bool = False):
+    # PERFORM THE OVERALL SOLVE.
+    def solve(self, brute: bool = False) -> None:
         for _ in range(3):
             self.solver_distinct()
             self.solver_eliminate()
         if brute:
             self.solve_brute()
 
-    def check_complete(self):
+    # CHECKS IF THE SUDOKU BOARD IS FILLED UP COMPLETELY
+    def check_complete(self) -> bool:
         if np.count_nonzero(self.board) == 81:
             return True
         else:
             return False
 
-    def solve_brute(self):
-        backup_board = np.copy(self.board)
+    # SOLVES BY GOING THROUGH THE ALLOWED NUMBERS FOR A CELL
+    # AND SOLVING BY DISTINCT AND ELIMINATE FUNCTION
+    # TILL A COMPLETE BOARD IS FOUND - WHICH IS THE SOLUTION
+    def solve_brute(self) -> None:
+        backup_board: np.ndarray = np.copy(self.board)
         for i in range(9):
             for j in range(9):
                 if self.board[i][j] == 0:
-                    allowed_numbers = self.get_allowed_numbers(row=i, col=j)
+                    allowed_numbers: list = self.get_allowed_numbers(row=i, col=j)
                     for num in allowed_numbers:
                         self.board[i][j] = num
                         self.solve(brute=False)
